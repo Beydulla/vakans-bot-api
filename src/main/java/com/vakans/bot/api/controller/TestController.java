6de2b2f8195765901dc7b471b67df52b3180349a
beydulla <zeynalov.beydulla97@gmail.com>
2021-11-28 17:49:52 +0400

    test controller

---
+++ src/main/java/com/vakans/bot/api/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.vakans.bot.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String testString(){
+        return "Test successful!";
+    }
+}
